b11339dd09ca995cf199d3a6d0b9d4545a60532b
StellaOnyekwelu <105196222+StellaOnyekwelu@users.noreply.github.com>
2022-06-22 21:29:41 +0100

    initial commit

---
+++ name.js
@@ -0,0 +1,6 @@
+var username = prompt("Enter your name: ");
+var height = prompt("Enter your height: ");
+var country = prompt("Enter your country-name: ");
+
+
+alert("My name is  " +username + "\n"  + "Height is  " +height  + "\n"  + "Country is  "  +country);
